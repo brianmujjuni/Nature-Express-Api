c51740205e291c9ce66ea113a17807d47061fd50
Brian <mbrian@projectcode.ug>
2024-10-04 11:29:18 +0300

    write new tour to tours

--- app.js
+++ app.js
@@ -20,6 +20,22 @@ app.get("/api/v1/tours", (req, res) => {
 });
 
 app.post("/api/v1/tours", (req, res) => {
+  const newIdd = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newIdd }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
   res.status(201).json({ message: "You can post to this endpoint...." });
 });
 
