c0b139406ae147e0ec5da41abb6d6839b905d4df
Brian <mbrian@projectcode.ug>
2024-10-04 10:12:11 +0300

    format endpoint

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server side");
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({ message: "Hello from the server", app: "Natures" });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  res.status(201).json({ message: "You can post to this endpoint...." });
 });
 
 const port = 3000;
