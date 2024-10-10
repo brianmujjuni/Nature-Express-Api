5e48c2003d037efdffa68a13c12421b7e13211ed
Brian <mbrian@projectcode.ug>
2024-10-10 12:23:45 +0300

    change delete endpoint

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.patch("/api/v1/tours/:id", (req, res) => {
   });
 });
 
-app.delete("api/v1/tours/:id",(req,res)=>{
+app.delete("/api/v1/tours/:id",(req,res)=>{
   if(req.params.id * 1 > tours.length){
     return res.status(404).json({
       status: 'fail',
