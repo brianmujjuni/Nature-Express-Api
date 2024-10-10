d14bf6796605e469c37c340c2c8b9ddade7c2190
Brian <mbrian@projectcode.ug>
2024-10-10 12:16:01 +0300

    check for id in params before edit

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ app.get("/api/v1/tours/:id", (req, res) => {
 });
 
 app.patch("/api/v1/tours/:id", (req, res) => {
+  if(req.params.id * 1 > tours.length){
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
   res.status(200).json({
     message: "success",
     data: {
@@ -55,6 +61,7 @@ app.patch("/api/v1/tours/:id", (req, res) => {
     },
   });
 });
+
 
 const port = 3000;
 
