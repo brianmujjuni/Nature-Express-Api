12abfc12657f57bfa29082b354b72fb1fa5be3eb
Brian <mbrian@projectcode.ug>
2024-10-12 12:13:05 +0300

    create sample model

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app')
+const app = require("./app");
 
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con =>{
+  // console.log(con.connections)
+  console.log('database connected successfully')
+});
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true,'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true,'A tour must have a price']
+  }
+})
+
+const Tour = mongoose.model('Tour',tourSchema)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
