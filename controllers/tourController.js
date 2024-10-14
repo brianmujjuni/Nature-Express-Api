260dca3f16704825912f71dfc98e4e14729eb27d
Brian <mbrian@projectcode.ug>
2024-10-14 11:28:20 +0300

    remove all unsed code for json file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,36 +31,21 @@ exports.getAllTours = (req, res) => {
     res.status(200).json({
       status: "success",
       time: req.requestTime,
-      results: tours.length,
-      data: {
-        tours,
-      },
+      // results: tours.length,
+      // data: {
+      //   tours,
+      // },
     });
   };
   
   exports.getTour = (req, res) => {
     
-    const tour = tours.find((el) => el.id === req.params.id * 1);
-    res.status(200).json({ status: "success", data: { tour } });
+  
+    res.status(200).json({ status: "success", data: '' });
   };
   
   exports.createTour = (req, res) => {
-    const newIdd = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newIdd }, req.body);
-  
-    tours.push(newTour);
-    fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      (err) => {
-        res.status(201).json({
-          status: "success",
-          data: {
-            tour: newTour,
-          },
-        });
-      }
-    );
+   
   };
   
   exports.updateTour = (req, res) => {
