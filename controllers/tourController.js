3851429b42ccba8f20c4189f2f75c6923cc3453f
Brian <mbrian@projectcode.ug>
2024-10-14 13:08:56 +0300

    update tour from db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,15 +25,14 @@ exports.getAllTours = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-
     const tour = await Tour.findById(req.params.id);
 
     res.status(200).json({ status: "success", data: tour });
   } catch (error) {
     res.status(404).json({
-      status: 'failed',
-      message: error
-    })
+      status: "failed",
+      message: error,
+    });
   }
 };
 
@@ -54,13 +53,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    message: "success",
-    data: {
-      tour: "Updated tour",
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      message: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error,
+    });
+  }
 };
 exports.deleteTour = (req, res) => {
   res.status(204).json({
