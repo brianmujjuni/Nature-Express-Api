2868f7a39e0090b77ae8367823b0d206a7828652
Brian <mbrian@projectcode.ug>
2024-10-15 13:06:35 +0300

    create middleware to get top 5 tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,12 @@ const Tour = require("../models/tourModel");
 //     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 //   );
 
+exports.aliasTopTours = (req,res,next)=>{
+  req.query.limit = '5'
+  req.query.sort = '-ratingAverage,price'
+  req.query.fields = 'name,price,ratingAverage,summary,difficulty'
+  next()
+}
 exports.getAllTours = async (req, res) => {
   try {
     //1)filtering
