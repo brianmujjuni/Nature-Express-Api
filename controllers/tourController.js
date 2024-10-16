87c317ac4137cb74b1ed99e24d3e9f3376bf7d78
Brian <mbrian@projectcode.ug>
2024-10-16 10:01:04 +0300

    create get monthly plan stats method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -137,3 +137,23 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async(req,res)=>{
+  try {
+    const year = req.params.year * 1
+    const plan = await Tour.aggregate({
+
+    })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan
+      }
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error,
+    });
+  }
+}
