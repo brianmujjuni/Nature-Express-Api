e9a2987b077e23c7e279f7b76ecb4b78f7325e75
Brian <mbrian@projectcode.ug>
2024-10-16 11:03:53 +0300

    presave middleware on tour creation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -6,6 +7,9 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
     trim: true,
+  },
+  slug:{
+    type: String
   },
   duration: {
     type: Number,
@@ -58,9 +62,15 @@ const tourSchema = new mongoose.Schema({
   toJSON: { virtuals: true},
   toObject: {virtuals: true}
 });
-
+//virtual
 tourSchema.virtual('durationWeeks').get(function(){
   return this.duration / 7
+})
+
+//Document Middleware: runs before .save() and .create()
+tourSchema.pre('save',function(next){
+  this.slug = slugify(this.name,{lower: true})
+  next()
 })
 
 const Tour = mongoose.model("Tour", tourSchema);
