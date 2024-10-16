40ff30ff4e89b5b0989917087be35823a331a0b5
Brian <mbrian@projectcode.ug>
2024-10-16 16:49:05 +0300

    create global error handler middlware

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
-
 const express = require("express");
 const morgan = require("morgan");
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const AppError = require("./utils/appError");
+const globalErrorHandler = require('./controllers/errorController')
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 //1) Middlewares
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-
 //mount routes
 app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users",userRouter);
+app.use("/api/v1/users", userRouter);
 //catch un avail routes
-app.all('*',(req,res,next)=>{
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`
-  // })
-  const err = new Error(`Can't find ${req.originalUrl} on this server`)
-  err.status = 'fail'
-  err.statusCode = 404
-  next(err)
-})
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
 
-app.use((err, req, res, next)=>{
-  err.statusCode = err.statuscode || 500
-  err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(globalErrorHandler);
 
-module.exports = app
-
+module.exports = app;
