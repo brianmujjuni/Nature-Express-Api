a247c73cb7c8073ed613887239f44c5f23405689
Brian <mbrian@projectcode.ug>
2024-10-17 10:57:09 +0300

    resolve bugs in error handler

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const AppError = require("./utils/appError");
-const globalErrorHandler = require('./controllers/errorController')
-
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1) Middlewares
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
@@ -22,12 +21,12 @@ app.use((req, res, next) => {
   next();
 });
 
-//mount routes
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
-//catch un avail routes
-app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
