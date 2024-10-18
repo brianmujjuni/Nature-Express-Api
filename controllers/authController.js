79483eca6e283fbeb40c12d060e1e980a25e5224
Brian <mbrian@projectcode.ug>
2024-10-18 16:09:45 +0300

    resolve password select bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,15 @@ const User = require("./../models/userModel");
 const catchAsysnc = require("./../utils/catchAsync");
 const AppError = require("../utils/appError");
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 exports.signup = catchAsysnc(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
   const newUser = await User.create({ name, email, password, passwordConfirm });
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: "Success",
     data: {
@@ -21,12 +24,18 @@ exports.signup = catchAsysnc(async (req, res, next) => {
 exports.login = catchAsysnc(async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
-   return next(new AppError("Please provide email and password", 400));
+    return next(new AppError("Please provide email and password", 400));
   }
-  
-  const user = await User.findOne({email}).seletc('+password')
+
+  const user = await User.findOne({ email }).select("+password");
+  const correct = await user.correctPassword(password, user.password);
+
+  if (!user || !correct) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+  const token = signToken(user._id);
 
   res.status(200).json({
-    "token":'jjjjj'
-  })
+    token: token,
+  });
 });
