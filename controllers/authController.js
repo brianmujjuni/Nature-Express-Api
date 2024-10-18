fe5195958e8e9fea55048f6e17b467f21ccb33dc
Brian <mbrian@projectcode.ug>
2024-10-18 15:29:26 +0300

    create login handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,14 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsysnc = require("./../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.signup = catchAsysnc(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
   const newUser = await User.create({ name, email, password, passwordConfirm });
-  const token = jwt.sign({id: newUser._id},process.env.JWT_SECRET,{
-    expiresIn: process.env.JWT_EXPIRES_IN
-  })
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: "Success",
     data: {
@@ -15,4 +16,14 @@ exports.signup = catchAsysnc(async (req, res, next) => {
       user: newUser,
     },
   });
+});
+
+exports.login = catchAsysnc(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(new AppError("Please provide email and password", 400));
+  }
+  res.status(200).json({
+    "token":'jjjjj'
+  })
 });
