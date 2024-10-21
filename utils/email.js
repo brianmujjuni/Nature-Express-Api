2e5d794106400334557921a2ec0da492b2fdb7c4
Brian <mbrian@projectcode.ug>
2024-10-21 12:56:51 +0300

    create mailoptions objects

--- utils/email.js
+++ utils/email.js
@@ -2,10 +2,20 @@ const nodemailer = require('nodemailer')
 
 const sendEmail = options => {
     const transpoter = nodemailer.createTransport({
-        service: 'Gmail',
+        // service: 'Gmail',
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth:{
             user: process.env.EMAIL_USERNAME,
             password: process.env.EMAIL_PASSWORD
         }
+        
     })
+    const mailOptions ={
+        from: 'Automex Technologies',
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+        // html:
+    }
 }
