0e82349fe2b447e1aa6e3f889ec53341588a502d
Brian <mbrian@projectcode.ug>
2024-10-21 15:15:23 +0300

    add user to get all tours in restrict middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
 router.route("/monthly-plan/:year").get(getMonthlyPlan);
-router.route("/").get(protect,restrictTo('admin','lead-guide'), getAllTours).post(createTour);
+router.route("/").get(protect,restrictTo('admin','lead-guide','user'), getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
