15a8e7cff2fa927abb366ccb7e2238e6ec5641e1
Brian <mbrian@projectcode.ug>
2024-10-22 12:52:36 +0300

    resolve login bugs

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,16 +52,19 @@ exports.signup = catchAsysnc(async (req, res, next) => {
 
 exports.login = catchAsysnc(async (req, res, next) => {
   const { email, password } = req.body;
+
+  // 1) Check if email and password exist
   if (!email || !password) {
-    return next(new AppError("Please provide email and password", 400));
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
   }
 
-  const user = await User.findOne({ email }).select("+password");
-  const correct = await user.correctPassword(password, user.password);
-
-  if (!user || !correct) {
-    return next(new AppError("Incorrect email or password", 401));
-  }
+  // 3) If everything ok, send token to client
   createSendToken(user, 200, res);
 });
 
