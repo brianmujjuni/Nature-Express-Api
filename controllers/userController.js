de82e04b1b37e4bf38340e083a1d8bb01a334398
Brian <mbrian@projectcode.ug>
2024-10-22 09:52:15 +0300

    create filte objects function to update user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,16 @@ const User = require("../models/userModel");
 const catchAsysnc = require("./../utils/catchAsync");
 const AppError = require("../utils/appError");
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsysnc(async (req, res, next) => {
   const users = await User.find();
   res.status(200).json({
@@ -12,11 +22,20 @@ exports.getAllUsers = catchAsysnc(async (req, res, next) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsysnc((req, res, next) => {
   if (req.body.password ?? req.body.passwordConfirm) {
     return next(new AppError("This route is not for password updates.", 400));
   }
-};
+  const filteredBody = filterObj(req.body, "name", "email");
+  const user = User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    user: req.user,
+  });
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ router.route("/login").post(login)
 router.route('/forgotPassword').post(forgotPassword)
 router.route('/resetPassword/:token').patch(resetPassword)
 router.route('/updateMyPassword').patch(protect,updatePassword)
-router.route('/updateMe',protect,updateMe)
+router.route('/updateMe').patch(protect,updateMe)
 
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
