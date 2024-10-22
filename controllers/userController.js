fd6805bb65823da84945a3d2a0e48f727e5fa155
Brian <mbrian@projectcode.ug>
2024-10-22 10:00:45 +0300

    resolve async bug fix on user update

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,18 +22,23 @@ exports.getAllUsers = catchAsysnc(async (req, res, next) => {
   });
 });
 
-exports.updateMe = catchAsysnc((req, res, next) => {
+exports.updateMe = catchAsysnc(async(req, res, next) => {
   if (req.body.password ?? req.body.passwordConfirm) {
     return next(new AppError("This route is not for password updates.", 400));
   }
+  //filter out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, "name", "email");
-  const user = User.findByIdAndUpdate(req.user._id, filteredBody, {
+
+  //update user documet
+  const updateUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
     new: true,
     runValidators: true,
   });
   res.status(200).json({
     status: "success",
-    user: req.user,
+    data: {
+      user: updateUser,
+    },
   });
 });
 exports.getUser = (req, res) => {
