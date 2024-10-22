3b869c107bb35e9b35f00659c32363c6ec54d477
Brian <mbrian@projectcode.ug>
2024-10-22 11:29:44 +0300

    add delete me route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,31 @@
 const express = require("express");
-const {getAllUsers,createUser,getUser,updateUser,deleteUser, updateMe} = require('../controllers/userController')
-const {signup, login,forgotPassword,resetPassword,protect, updatePassword} =require('./../controllers/authController')
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+  updateMe,
+  deleteMe,
+} = require("../controllers/userController");
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  protect,
+  updatePassword,
+} = require("./../controllers/authController");
 
 const router = express.Router();
 
-
-router.route('/signup').post(signup)
-router.route("/login").post(login)
-router.route('/forgotPassword').post(forgotPassword)
-router.route('/resetPassword/:token').patch(resetPassword)
-router.route('/updateMyPassword').patch(protect,updatePassword)
-router.route('/updateMe').patch(protect,updateMe)
+router.route("/signup").post(signup);
+router.route("/login").post(login);
+router.route("/forgotPassword").post(forgotPassword);
+router.route("/resetPassword/:token").patch(resetPassword);
+router.route("/updateMyPassword").patch(protect, updatePassword);
+router.route("/updateMe").patch(protect, updateMe);
+router.route("/deleteMe").delete(protect, deleteMe);
 
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
