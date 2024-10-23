8ef47a6a7647ddf71c2f25488ab87da965aacab0
Brian <mbrian@projectcode.ug>
2024-10-23 15:26:09 +0300

    create a pre find middleware for populate

--- models/tourModel.js
+++ models/tourModel.js
@@ -135,6 +135,14 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangesAt",
+  });
+  next();
+});
+
 // tourSchema.post(/^find/,function(doc,next){
 
 //   next()
