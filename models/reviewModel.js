617d290227d42bb52a780cb8281d16eaad7d6916
Brian <mbrian@projectcode.ug>
2024-10-23 18:23:09 +0300

    resove populate on reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,7 +35,10 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: "user",
-    select: "-__v ",
+    select: "name photo",
+  }).populate({
+    path: "tour",
+    select: "name ",
   });
   next();
 });
