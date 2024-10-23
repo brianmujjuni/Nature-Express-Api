029776241ed27bdbb83a95488bc890fd572ec11c
Brian <mbrian@projectcode.ug>
2024-10-23 18:13:08 +0300

    populate user on review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,5 +32,13 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "-__v ",
+  });
+  next();
+});
+
 const Review = mongoose.model("Review", reviewSchema);
 module.exports = Review;
