5b1707679d0ec84b4e7292c2e7038741fc528221
Brian <mbrian@projectcode.ug>
2024-10-24 08:59:17 +0300

    restracture create tour review in tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,10 +10,15 @@ const {
   getMonthlyPlan,
 } = require("../controllers/tourController");
 const { protect, restrictTo } = require("../controllers/authController");
-const { createReview } = require("../controllers/reviewController");
+const reviewRouter = require("../routes/reviewRoutes")
+// const { createReview } = require("../controllers/reviewController");
 
 const router = express.Router();
 
+router.use('/:tourId/reviews',reviewRouter)
+// router
+//   .route("/:tourId/reviews")
+//   .post(protect, restrictTo("user"), createReview);
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
 router.route("/monthly-plan/:year").get(getMonthlyPlan);
@@ -23,7 +28,5 @@ router
   .post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
-router
-  .route("/:tourId/reviews")
-  .post(protect, restrictTo("user"), createReview);
+
 module.exports = router;
