8a73facb5ef86b83890841daf22bca6364154a73
Brian <mbrian@projectcode.ug>
2024-10-24 15:40:37 +0300

    create middleware to assign params to user id

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsysnc(async (req, res, next) => {
   if (req.body.password ?? req.body.passwordConfirm) {
     return next(new AppError("This route is not for password updates.", 400));
