eb5f7475d4159a62adadbbd907aa1f476b1cb4f0
Brian <mbrian@projectcode.ug>
2024-10-24 10:50:20 +0300

    protect delete tour route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,12 +10,12 @@ const {
   getMonthlyPlan,
 } = require("../controllers/tourController");
 const { protect, restrictTo } = require("../controllers/authController");
-const reviewRouter = require("../routes/reviewRoutes")
+const reviewRouter = require("../routes/reviewRoutes");
 // const { createReview } = require("../controllers/reviewController");
 
 const router = express.Router();
 //nested create review route
-router.use('/:tourId/reviews',reviewRouter)
+router.use("/:tourId/reviews", reviewRouter);
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
@@ -24,7 +24,10 @@ router
   .route("/")
   .get(protect, restrictTo("admin", "lead-guide", "user"), getAllTours)
   .post(createTour);
-router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
-
+router
+  .route("/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo("admin"), deleteTour);
 
 module.exports = router;
