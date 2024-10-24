6df9deabcb055e353592cb578c286637487457ef
Brian <mbrian@projectcode.ug>
2024-10-24 11:42:59 +0300

    add update user handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel");
 const catchAsysnc = require("./../utils/catchAsync");
 const AppError = require("../utils/appError");
-const factory = require("./handlerFactory")
+const factory = require("./handlerFactory");
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -65,11 +65,5 @@ exports.createUser = (req, res) => {
     message: "This route is not yet defined!",
   });
 };
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
-};
-
-exports.deleteUser = factory.deleteOne(User)
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
