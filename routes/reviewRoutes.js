024e992a98c0cc8ded374e6d317c3404a6115b54
Brian <mbrian@projectcode.ug>
2024-10-24 11:07:38 +0300

    create delete handler for deleting single user

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const {
   getAllReviews,
   createReview,
+  deleteReview,
 } = require("../controllers/reviewController");
 const { protect, restrictTo } = require("../controllers/authController");
 
@@ -11,5 +12,5 @@ router
   .route("/")
   .get(protect, getAllReviews)
   .post(protect, restrictTo("user"), createReview);
-
+router.route("/:id").delete(protect, deleteReview);
 module.exports = router;
