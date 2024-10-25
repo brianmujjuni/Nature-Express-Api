ce9f918034a79134e1eb128396a825775db39009
Brian <mbrian@projectcode.ug>
2024-10-25 15:14:34 +0300

    update avrageRating using middleware

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -61,12 +62,32 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
       },
     },
   ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: stats[0].nRating,
+      ratingAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: 4.5,
+      ratingAverage: 4.5,
+    });
+  }
 };
 
 reviewSchema.post("save", function () {
   //this point to the current review
   this.constructor.calcAverageRatings(this.tour);
-  
 });
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
 const Review = mongoose.model("Review", reviewSchema);
 module.exports = Review;
