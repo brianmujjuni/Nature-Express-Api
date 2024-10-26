6a1f0dc0390b09d81f45e71917875d3fa35510c9
Brian <mbrian@projectcode.ug>
2024-10-26 15:05:57 +0300

    server views & public files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -10,9 +11,12 @@ const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
-const reviewRouter = require("./routes/reviewRoutes")
+const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 //Set Securuty HTTP header
 app.use(helmet());
@@ -53,7 +57,8 @@ app.use(
 );
 
 //server static files
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname,"public")))
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -63,7 +68,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/reviews",reviewRouter)
+app.use("/api/v1/reviews", reviewRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
