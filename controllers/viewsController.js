9e10b64a4137378db2b31e76ce33fbf81e27b813
Brian <mbrian@projectcode.ug>
2024-10-28 15:32:09 +0300

    fetch single tour

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -9,8 +9,14 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const slug = req.params;
+  const tour = await Tour.findOne({ slug: slug }).populate({
+    path: "reviews",
+    fileds: "review rating user",
+  });
   res.status(200).render("tour", {
     title: "The Forest Hiker Tour",
+    tour
   });
-};
+});
