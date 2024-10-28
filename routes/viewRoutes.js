2765a5a21a78651c90228e5120bf454b7e1377e7
Brian <mbrian@projectcode.ug>
2024-10-28 11:44:01 +0300

    restracture overview routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,13 +3,6 @@ const { getOverview, getTour } = require("../controllers/viewsController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res
-    .status(200)
-    .render("base", { tour: "Mujjuni Brian", developer: "Project Code" });
-});
-
-router.get("/overview", getOverview);
-
+router.get("/", getOverview);
 router.get("/tour", getTour);
 module.exports = router;
