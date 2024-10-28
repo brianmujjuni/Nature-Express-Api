7216be0de458f46c7abf3fde6e41e47e4c16ac40
Brian <mbrian@projectcode.ug>
2024-10-28 18:53:43 +0300

    populate tour details

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,13 +10,14 @@ exports.getOverview = catchAsync(async (req, res) => {
 });
 
 exports.getTour = catchAsync(async (req, res) => {
-  const slug = req.params;
-  const tour = await Tour.findOne({ slug: slug }).populate({
+
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: "reviews",
     fileds: "review rating user",
   });
+
   res.status(200).render("tour", {
     title: "The Forest Hiker Tour",
-    tour
+    tour: tour
   });
 });
