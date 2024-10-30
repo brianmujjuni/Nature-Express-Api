52d7590c551c2ae5c952c8018c3510f1020ab634
Brian <mbrian@projectcode.ug>
2024-10-30 21:58:42 +0300

    disable zoom

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -2,12 +2,42 @@
 const locations = JSON.parse(document.getElementById("map").dataset.locations);
 
 mapboxgl.accessToken =
-"pk.eyJ1IjoiYnJpYW5tdWpqdW5pIiwiYSI6ImNtMnZqczhwZzBmcHIycXNoNWNhamUxNDMifQ.x-DYXkpc8_yIALACV0dKPw";
+  "pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A";
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
-    zoom: 1,
-    center: [30, 15]
+  container: "map",
+  style: "mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy",
+  scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  //create marker
+  const el = document.createElement("div");
+  el.className = "marker";
+  //add amrker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: "bottom",
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+  //add popup
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description} </p>`).addTo(map);
+  //extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
 });
