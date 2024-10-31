17e5278a5d1196c960a1a08ba1e971c190d70046
Brian <mbrian@projectcode.ug>
2024-10-31 12:27:36 +0300

    create login route

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require("../models/tourModel");
 const catchAsync = require("../utils/catchAsync");
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res,next) => {
   const tours = await Tour.find();
   res.status(200).render("overview", {
     title: "All Tours",
@@ -9,7 +9,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res,next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: "reviews",
     fileds: "review rating user",
@@ -23,3 +23,8 @@ exports.getTour = catchAsync(async (req, res) => {
     });
 });
     
+exports.getLoginFrom = (req,res,next)=>{
+  res.status(200).render('login',{
+    title: 'Log into your account'
+  })
+}
